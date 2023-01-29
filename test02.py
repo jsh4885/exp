f48326856abf3474ca09697f1d31844014c50fec
jsh4885 <kg508175@gmail.com>
2023-01-29 23:25:40 +0900

    test02

---
+++ test02.py
@@ -0,0 +1,6 @@
+import olefile
+f = olefile.OleFileIO('test02.hwp')
+encoded_text = f.openstream('PrvText').read()
+decoded_text = encoded_text.decode('UTF-16')
+print(decoded_text)
+print(len('decoded_text'))
