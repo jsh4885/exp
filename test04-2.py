1069f0a3419529577fdbb253f95b8922236e3cda
jsh4885 <kg508175@gmail.com>
2023-02-07 20:45:30 +0900

    한글 파일 끼워넣기(첨부)

---
+++ test04-2.py
@@ -0,0 +1,27 @@
+import os
+import win32com.client as win32
+hwp = win32.gencache.EnsureDispatch("HWPFrame.HwpObject")
+hwp.Run("FileNew")
+hwp.RegisterModule("FilePathCheckDLL", "SecurityModule")
+hwp.Open(r"C:\Users\JSH\Desktop\코딩\exp\상장샘플.hwp")
+
+BASE_DIR = r"C:\Users\JSH\Desktop\코딩\exp\첨부"
+첨부파일리스트 = os.listdir(r"C:\Users\JSH\Desktop\코딩\exp\첨부")
+
+def 첨부삽입(path):
+    hwp.HAction.GetDefault("InsertFile", hwp.HParameterSet.HInsertFile.HSet)
+    hwp.HParameterSet.HInsertFile.filename = path
+    hwp.HParameterSet.HInsertFile.KeepSection = 1
+    hwp.HParameterSet.HInsertFile.KeepCharshape = 1
+    hwp.HParameterSet.HInsertFile.KeepParashape = 1
+    hwp.HParameterSet.HInsertFile.KeepStyle = 1
+    hwp.HAction.Execute("InsertFile", hwp.HParameterSet.HInsertFile.HSet)
+    return
+
+hwp.MovePos(3)
+
+for i in 첨부파일리스트:
+    첨부삽입(os.path.join(BASE_DIR, i))
+    hwp.MovePos(3)
+
+hwp.Quit()
