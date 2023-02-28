8d698c4fdec644829225df79e9c77773cecfc709
jsh4885 <kg508175@gmail.com>
2023-02-28 10:50:35 +0900

    구글 검색창에 '군산' 검색

--- test05.py
+++ test05.py
@@ -8,5 +8,5 @@ driver.get('https://www.google.com/')
 driver.find_element(By.CLASS_NAME, value='gLFyf').send_keys('군산')
 
 driver.find_element(By.XPATH, value='/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]').click()
-
+a
 input()
